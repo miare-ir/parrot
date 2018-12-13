ab3e0c565c5b7dd30dc220b006a1a0b5da874e1c
pdrum <pedram.esmaeeli@gmail.com>
2018-12-13 13:21:14 +0330

    feat: Initial structure

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='parrot',
+    version='1.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='Parrot facilitates replaying requests arrived at a server on another server',
+    install_requires=[
+        'django>=1.11.0',
+        'djangorestframework',
+    ],
+    tests_require=['Django>=1.8'],
+    long_description=README,
+    url='https://github.com/miare-ir/parrot',
+    author='Miare',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
